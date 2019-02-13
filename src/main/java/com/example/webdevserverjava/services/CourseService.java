7b47c06235629a4d9eeb0a4949089e91fd6ce1c9
preyank <preyank@ccs.neu.edu>
2019-02-13 01:04:17 -0500

    created api/courses mapping

--- src/main/java/com/example/webdevserverjava/services/CourseService.java
+++ src/main/java/com/example/webdevserverjava/services/CourseService.java
@@ -1,6 +1,22 @@
 package com.example.webdevserverjava.services;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.webdevserverjava.model.User;
+
+import com.example.webdevserverjava.model.Course;;
+
+@RestController
 public class CourseService {
 	
+	@PostMapping("/api/courses")
+	public Course createCourse(@RequestBody User newCourse,
+			HttpSession session) {
+		return null;
+	}
 
 }
