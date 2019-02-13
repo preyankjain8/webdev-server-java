310b55cfd39f630aa6f417dd3eac377484f85213
preyank <preyank@ccs.neu.edu>
2019-02-12 22:12:29 -0500

    implemented faculty.java

---
+++ src/main/java/com/example/webdevserverjava/model/Faculty.java
@@ -0,0 +1,40 @@
+package com.example.webdevserverjava.model;
+
+public class Faculty {
+	private Integer id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
