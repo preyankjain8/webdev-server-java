00c991d47bb5a9dd509aade59a7934f4e3c5cf5c
preyank <preyank@ccs.neu.edu>
2019-02-15 02:58:28 -0500

    added update functionality to topicService

--- src/main/java/com/example/webdevserverjava/services/LessonService.java
+++ src/main/java/com/example/webdevserverjava/services/LessonService.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.webdevserverjava.model.Course;
 import com.example.webdevserverjava.model.Lesson;
 import com.example.webdevserverjava.model.Module;
 
@@ -64,6 +66,19 @@ public class LessonService {
 		}
 		return null;
 	}
+	
+	@PutMapping("/api/lesson/{lid}")
+	public Lesson updateLesson(@PathVariable("lid") Integer lessonId,
+			@RequestBody Lesson lesson){
+		for(Lesson l : lessonList) {
+			if(l.getId().equals(lessonId)) {
+				l = lesson;
+				return l;
+			}
+		}
+		return null;
+	}
+	
 	
 	@DeleteMapping("/api/lesson/{lid}")
 	public void DeleteLesson(@PathVariable("lid") Integer lessonId){
